e09dccde0e612e01331f227d93d599878f130fa9
Nicolas <nicolas.gagnon.3@gmail.com>
2015-04-12 18:40:24 -0400

    J'ai le poil du cul propre

---
+++ app/src/main/java/lesdevoreurs/bon_manger/BigOvenWebAPI.java
@@ -0,0 +1,114 @@
+package lesdevoreurs.bon_manger;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by Nicolas on 2015-02-19.
+ */
+public class BigOvenWebAPI {
+    ArrayList<String> titres;
+    ArrayList<String> cuisines;
+    ArrayList<String> categories;
+    ArrayList<String> sousCategories;
+    ArrayList<Drawable> images;
+
+    public BigOvenWebAPI(String query){
+
+        String url = "http://api.bigoven.com/recipes?title_kw="+query+"&pg=1&rpp=100&api_key=dvxRg7vK4t5RBlTap04zYHqbu08e374G";
+        titres = new ArrayList<String>();
+        images = new ArrayList<Drawable>();
+        cuisines = new ArrayList<String>();
+        categories = new ArrayList<String>();
+        sousCategories = new ArrayList<String>();
+
+        //http://openclassrooms.com/courses/structurez-vos-donnees-avec-xml/dom-exemple-d-utilisation-en-java
+        //Etape 1 : récupération d'une instance de la classe "DocumentBuilderFactory"
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try {
+            HttpEntity page = getHttp(url);
+            String contenu  = EntityUtils.toString(page, HTTP.UTF_8);
+            URL urlXML = new URL(url);
+
+            //Etape 2 : création d'un parseur
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+            //Etape 3 : création d'un Document
+            final Document document= builder.parse(url);
+            //Etape 4 : récupération de l'Element racine
+            final Element racine = document.getDocumentElement();
+            //Etape 5 : récupération des recettes
+            //final NodeList racineNoeuds = racine.getChildNodes();
+            //final int nbRacineNoeuds = racineNoeuds.getLength();
+            final NodeList racineNoeuds = racine.getElementsByTagName("Results");
+            final int nbRacineNoeuds = racineNoeuds.getLength();
+            final Element racineNoeudsNoeuds = (Element) racineNoeuds.item(0);
+            final NodeList resultNoeuds = racineNoeudsNoeuds.getElementsByTagName("RecipeInfo");
+            final int nbResultNoeuds = resultNoeuds.getLength();
+            Log.d("Racine",resultNoeuds.item(0).getNodeName());
+            Log.d("WEB", "Nombre de recettes: " + nbResultNoeuds);
+
+            for(int i =0; i<nbResultNoeuds; i++){
+                if(resultNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    final Element recette = (Element) resultNoeuds.item(i);
+                    //Etape 6 : récupération du nom et du prénom
+                    final Element title = (Element) recette.getElementsByTagName("Title").item(0);
+                    final Element cuisine = (Element) recette.getElementsByTagName("Cuisine").item(0);
+                    final Element categorie = (Element) recette.getElementsByTagName("Category").item(0);
+                    final Element souscategorie = (Element) recette.getElementsByTagName("Subcategory").item(0);
+                    titres.add(title.getTextContent());
+                    if(cuisine!=null)
+                        cuisines.add(cuisine.getTextContent());
+                    else
+                        cuisines.add("");
+                    categories.add(categorie.getTextContent());
+                    sousCategories.add(souscategorie.getTextContent());
+                }
+            }
+        } catch (IOException e) {
+            Log.d("Web ", "Erreur: "+e.getMessage());
+        } catch (ParserConfigurationException e) {
+            Log.d("Parser ", "Erreur: " + e.getMessage());
+        } catch (SAXException e) {
+            Log.d("SAX ", "Erreur: " + e.getMessage());
+        }
+    }
+
+    public HttpEntity getHttp(String url) throws ClientProtocolException, IOException{
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet http = new HttpGet(url);
+        HttpResponse response = httpClient.execute(http);
+        return response.getEntity();
+    }
+
+    private Drawable loadHttpImage(String url) throws ClientProtocolException, IOException {
+        InputStream is = getHttp(url).getContent();
+        Drawable d = Drawable.createFromStream(is, "src");
+        return d;
+    }
+}
+
+
--- app/src/main/java/lesdevoreurs/bon_manger/MainActivity.java
+++ app/src/main/java/lesdevoreurs/bon_manger/MainActivity.java
@@ -1,17 +1,42 @@
 package lesdevoreurs.bon_manger;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    Button btn;
+    EditText edR;
+    ListView listv;
+    TextView titre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+
+        btn = (Button)findViewById(R.id.docherche);
+        edR = (EditText)findViewById(R.id.cherche);
+        listv = (ListView)findViewById(R.id.activity_list);
+        titre = (TextView)findViewById(R.id.activity_title);
+
+        titre.setText("Recherche de recettes");
+        btn.setOnClickListener(this);
     }
 
 
@@ -35,5 +60,77 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String recherche = edR.getText().toString();
+        if (!recherche.matches("")) {
+            Toast.makeText(this, "Chargement des donnees du Web", Toast.LENGTH_SHORT).show();
+            new DownloadWebTask().execute();
+        }
+    }
+
+    public class DownloadWebTask extends AsyncTask<Void, Void, BigOvenWebAPI>{
+
+        @Override
+        protected BigOvenWebAPI doInBackground(Void... params) {
+            String query = edR.getText().toString().replace(" ", "%20");
+            BigOvenWebAPI web = new BigOvenWebAPI(query);
+            return web;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(BigOvenWebAPI bigovenwebapi) {
+            ArrayList<String> titres = bigovenwebapi.titres;
+            MyAdapter adapter = new MyAdapter(titres);
+            listv.setAdapter(adapter);
+        }
+    }
+
+    public class MyAdapter extends BaseAdapter{
+        ArrayList<String> titres;
+        LayoutInflater inflater;
+
+        public MyAdapter(ArrayList<String> titres){
+            inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.titres= titres;
+        }
+
+        @Override
+        public int getCount() {
+            return titres.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+
+            if(v==null){
+                v = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            }
+
+            TextView titre = (TextView)v.findViewById(android.R.id.text1);
+            titre.setText(titres.get(position));
+
+            return v;
+        }
+
+
     }
 }
