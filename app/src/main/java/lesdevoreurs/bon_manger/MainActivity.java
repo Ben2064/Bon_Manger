0b3da9316e98bcf0bbcb65708a49de3bd7bb33f3
wx444 <virgilelaforest@hotmail.com>
2015-04-17 14:55:46 -0400

    back button fonctionne avec les fragments

--- app/src/main/java/lesdevoreurs/bon_manger/MainActivity.java
+++ app/src/main/java/lesdevoreurs/bon_manger/MainActivity.java
@@ -264,7 +264,7 @@ public class MainActivity extends Activity  {
         if (fragment != null) {
             FragmentManager fragmentManager = getFragmentManager();
             fragmentManager.beginTransaction()
-                    .replace(R.id.frame_container, fragment).commit();
+                    .replace(R.id.frame_container, fragment).addToBackStack("back").commit();
 
             // update selected item and title, then close the drawer
             mDrawerList.setItemChecked(position, true);
