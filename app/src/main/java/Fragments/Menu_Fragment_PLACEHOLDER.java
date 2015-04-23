65ce7e65ca8d11ee5965af5c85432a47543d8633
ben2064 <benzolcyclohexa@gmail.com>
2015-04-23 00:07:19 -0400

    Calendrier mockup and shit

--- app/src/main/java/Fragments/Menu_Fragment_PLACEHOLDER.java
+++ app/src/main/java/Fragments/Menu_Fragment_PLACEHOLDER.java
@@ -1,5 +1,6 @@
 package Fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -7,6 +8,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -16,6 +20,10 @@ import lesdevoreurs.bon_manger.R;
  * Created by virgile on 17/04/2015.
  */
 public class Menu_Fragment_PLACEHOLDER extends Fragment {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
     public Menu_Fragment_PLACEHOLDER() {
     }
 
@@ -40,6 +48,25 @@ public class Menu_Fragment_PLACEHOLDER extends Fragment {
 
         View rootView = inflater.inflate(R.layout.menu_layout_placeholder, container, false);
 
+        //Crée les items du spinner, ceci est du placeholder material solide
+        spinner = (Spinner) rootView.findViewById(R.id.calendar_date_select);
+        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.rolldown_date, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         return rootView;
+    }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
     }
 }
